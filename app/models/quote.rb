4f9f6c30e9b239304616a7a649a207802b60d70d
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-24 19:15:02 -0300

    Relacionamento com company

--- app/models/quote.rb
+++ app/models/quote.rb
@@ -1,6 +1,7 @@
 class Quote < ApplicationRecord
   validates :name, presence: true
+  belongs_to :company
 
   scope :ordered, -> { order(id: :desc) }
-  broadcasts_to -> (quote) { "quotes" }, inserts_by: :prepend
+  broadcasts_to -> (quote) { [quote.company, "quotes"] }, inserts_by: :prepend
 end
