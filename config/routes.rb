47f781504e45e917fe6a63a5d030e1e45f490811
kayke simao <89744379+KaykeS8@users.noreply.github.com>
2024-01-27 20:21:37 -0300

    Resources para line_item_date adiciondo

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users 
-  resources :quotes
+  resources :quotes do 
+    resources :line_item_dates, except: %i[ index show]
+  end
   root to: 'pages#home'
 end
